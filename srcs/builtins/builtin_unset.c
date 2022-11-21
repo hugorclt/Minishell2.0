82d903e1a3db7bec072e36dc52183043d6347e58
uninstall-lea <lea.bisson@gmail.com>
2022-11-21 17:05:20 +0100

    [update] wip builtin_unset

--- srcs/builtins/builtin_unset.c
+++ srcs/builtins/builtin_unset.c
@@ -6,7 +6,19 @@
 /*   By: lbisson <lbisson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 00:05:09 by lbisson           #+#    #+#             */
-/*   Updated: 2022/11/20 00:05:10 by lbisson          ###   ########.fr       */
+/*   Updated: 2022/11/20 18:29:14 by lbisson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+void	builtin_unset(char **arg)
+{
+	if (!arg[1])
+	{
+		dprintf(STDERR, "unset: not enough arguments\n");
+		update_last_cmd_status(FAILURE);
+		return ;
+	}
+	
+}
--- srcs/utils/error.c
+++ srcs/utils/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lbisson <lbisson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 19:19:48 by lbisson           #+#    #+#             */
-/*   Updated: 2022/11/19 10:46:40 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/11/19 22:15:14 by lbisson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	error_parsing(char *msg)
 {
-	ft_putstr_fd("mimishell: syntax error near unexpected token " 
-				RED "'%s' \n" RESET, msg);
+	dprintf(STDERR, "mimishell: syntax error near unexpected token "
+			RED "'%s' \n" RESET, msg);
 	free_all(FREE);
 }
