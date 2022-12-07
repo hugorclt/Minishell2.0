813288dcc43c66099f457c382ba6e106a9310f13
uninstall-lea <lea.bisson@gmail.com>
2022-12-07 23:09:01 +0100

    [fix] Adrian raconte n'importe quoi

--- srcs/exec/builtins/builtin_export.c
+++ srcs/exec/builtins/builtin_export.c
@@ -6,30 +6,44 @@
 /*   By: lbisson <lbisson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 23:54:08 by lbisson           #+#    #+#             */
-/*   Updated: 2022/12/07 22:44:32 by lbisson          ###   ########.fr       */
+/*   Updated: 2022/12/07 23:08:09 by lbisson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	print_export(void)
-{
-	t_list *env;
-
-	env = *_list();
-	
-}
-
 static void	check_export_error(char **arg)
 {
 	if (!arg[1])
-		print_export();
+		update_last_cmd_status(FAILURE);
 	else if (ft_isalpha(arg[1][0]) == FALSE && arg[1][0] != '_')
 	{
 		dprintf(STDERR, "mimishell: export: '%c': not a valiid identifier \n",
 						arg[1][0]);
 		update_last_cmd_status(FAILURE);
 	}
+}
+
+static char	*init_key(char *str, char *val, int equal_index)
+{
+	char	*key;
+	
+	if (!equal_index || *val =='\0')
+		key = ft_strdup(str);
+	else
+		key = ft_substr(str, 0, equal_index);
+	return (key);	
+}
+
+static char	*init_value(char *str, int equal_index)
+{
+	char	*value;
+	
+	if (!equal_index)
+		value = NULL;
+	else
+		value = ft_substr(str, equal_index + 1, -1);
+	return (value);
 }
 
 static void	export_key_and_value(char *str)
@@ -39,16 +53,8 @@ static void	export_key_and_value(char *str)
 	char	*val;
 
 	equal_index = ft_strchr_index(str, '=');
-	if (!equal_index)
-	{
-		key = ft_strdup(str);
-		val = NULL;
-	}
-	else
-	{
-		key = ft_substr(str, 0, equal_index);
-		val = ft_substr(str, equal_index + 1, -1);
-	}
+	val = init_value(str, equal_index);
+	key = init_key(str, val, equal_index);
 	if (env_get_key(key))
 		env_change_value(key, val);
 	else
