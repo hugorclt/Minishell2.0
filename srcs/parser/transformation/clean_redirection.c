02f540017707c8e6e3e60854a53bf72fb073b88a
hugorclt <hugorecolet@gmail.com>
2022-12-15 17:55:56 +0100

    [update]leaks

--- srcs/parser/transformation/clean_redirection.c
+++ srcs/parser/transformation/clean_redirection.c
@@ -6,7 +6,7 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 07:22:19 by hrecolet          #+#    #+#             */
-/*   Updated: 2022/12/15 17:03:13 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/12/15 17:55:21 by hrecolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ char	**clean_redirection(char **str, int nb_in, int nb_out)
 
 	i = 0;
 	j = 0;
-	printf("mallocvalue: %d\n",(tab_len(str) - (nb_in * 2 + nb_out * 2)) + 1);
 	ret = malloc(sizeof(char *) * ((tab_len(str) - (nb_in * 2 + nb_out * 2)) + 1));
 	if (!ret)
 		return (NULL);
@@ -36,6 +35,6 @@ char	**clean_redirection(char **str, int nb_in, int nb_out)
 		i++;
 	}
 	ret[j] = NULL;
-	printf("jvalue: %d\n", j);
+	free_matrix(str);
 	return (ret);
 }
