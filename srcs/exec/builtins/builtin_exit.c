44412697c3bb0f15fee80ab58ff31f395738dfeb
uninstall-lea <lea.bisson@gmail.com>
2022-12-19 17:47:29 +0100

    [fix] exit status when only exit

--- srcs/exec/builtins/builtin_exit.c
+++ srcs/exec/builtins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: lbisson <lbisson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 18:33:26 by lbisson           #+#    #+#             */
-/*   Updated: 2022/12/18 14:11:46 by lbisson          ###   ########.fr       */
+/*   Updated: 2022/12/19 17:45:50 by lbisson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static void	check_error_exit(char **arg)
 	int	status;
 
 	if (!arg[1])
-		return ;
+		return (update_last_cmd_status(SUCCESS));
 	status = ft_atoi(arg[1]);
 	if (arg[1] && is_num_arg(arg[1]) == FALSE)
 		exit_non_num_arg(arg[1]);
